eeb82edea67dfbe3b9e4bae2f295f707cb504ac7
AbhishekkGautam <46835940+AbhishekkGautam@users.noreply.github.com>
2022-03-29 05:02:42 +0530

    added boilerplate react & backend code

---
+++ src/backend/db/videos.js
@@ -0,0 +1,80 @@
+/**
+ * Videos Database can be added here.
+ * You can add videos of your wish with different attributes
+ * */
+
+export const videos = [
+  {
+    _id: "No8qdcVYiQw",
+    title: "Getting started with HTML5",
+    channelName: "Tanay Pratap",
+    views: 40143,
+    uploadedOn: "Nov 22, 2020",
+    categoryName: "HTML",
+    avatar:
+      "https://yt3.ggpht.com/ytc/AKedOLQnvobEXpzHCxB1g-q2aXbRDzSaznFVHo5OdFML=s48-c-k-c0x00ffffff-no-rj",
+    description:
+      "Welcome to neogcamp's levelZero journey. You don't know anything about web development? Good, you have come to the right place. Not only you'll learn the language of the web: HTML, you'll learn how to make your site pretty with CSS. When we have learned everything we will put together a decent profile to host your Internet adventures.",
+  },
+  {
+    _id: "jS4aFq5-91M",
+    title: "HTML Full Course - Build a Website Tutorial",
+    channelName: "freeCodeCamp.org",
+    views: 5295078,
+    uploadedOn: "Sept 18, 2018",
+    categoryName: "HTML",
+    avatar:
+      "https://yt3.ggpht.com/ytc/AKedOLRyPFojwY3CXV5ks5TwPrt2VifQn-nYNfkgLvVPkw=s48-c-k-c0x00ffffff-no-rj",
+    description:
+      "Learn the basics of HTML5 and web development in this awesome course for beginners. ",
+  },
+  {
+    _id: "1PnVor36_40",
+    title: "Learn CSS in 20 Minutes",
+    channelName: "Web Dev Simplified",
+    views: 808418,
+    uploadedOn: "Aug 23, 2018",
+    categoryName: "CSS",
+    avatar:
+      "https://yt3.ggpht.com/ytc/AKedOLQpvSjzSCSo8ZKCjBZS7TRX7omb_kyQirh2zgEY=s48-c-k-c0x00ffffff-no-rj",
+    description:
+      "In this video we will cover everything you need to know to get up and running with CSS in only 20 minutes. We will cover CSS syntax, how to add CSS to your HTML, CSS colors, CSS units, the box model, and best practices for CSS walking through a full example of CSS being used to style an HTML page. By the end of this video you will know enough about CSS to style any basic web pages in your own projects!",
+  },
+  {
+    _id: "PkZNo7MFNFg",
+    title: "Learn JavaScript - Full Course for Beginners",
+    channelName: "freeCodeCamp.org",
+    views: 8848594,
+    uploadedOn: "Dec 10, 2018",
+    categoryName: "Javascript",
+    avatar:
+      "https://yt3.ggpht.com/ytc/AKedOLRyPFojwY3CXV5ks5TwPrt2VifQn-nYNfkgLvVPkw=s48-c-k-c0x00ffffff-no-rj",
+    description:
+      "This complete 134-part JavaScript tutorial for beginners will teach you everything you need to know to get started with the JavaScript programming language.",
+  },
+  {
+    _id: "QCRpVw2KXf8",
+    title:
+      "SHORTEST JS Program 🔥window & this keyword | Namaste JavaScript Ep. 5",
+    channelName: "Akshay Saini",
+    views: 136615,
+    uploadedOn: "Oct 23, 2020",
+    categoryName: "Javascript",
+    avatar:
+      "https://yt3.ggpht.com/ytc/AKedOLR8gqN3fHHNYbehMcsJ49rapBPhJMGPYSrl6YQyNg=s48-c-k-c0x00ffffff-no-rj",
+    description:
+      "Let's check out the Shortest Program in JavaScript and more about the window and this keyword. Understand how the Global Execution Context is created, global object, and this keyword is created in JS. We'll also talk about what happens under the hood of the JS Engine when you execute this shortest code in the browser. ",
+  },
+  {
+    _id: "yLZazznWoAs",
+    title: "JavaScript in the browser!",
+    channelName: "Tanay Pratap",
+    views: 50654,
+    uploadedOn: "Nov 28, 2020",
+    categoryName: "Javascript",
+    avatar:
+      "https://yt3.ggpht.com/ytc/AKedOLQnvobEXpzHCxB1g-q2aXbRDzSaznFVHo5OdFML=s48-c-k-c0x00ffffff-no-rj",
+    description:
+      "Welcome to neogcamp. We have learned programming and web basics in previous lessons. This time we will use both things to create a dynamic app in a web browser. You'll learn about DOM scripting i.e. JS in the browser, calling servers, and getting data from there, taking user input and showing user output, and many more things.",
+  },
+];
